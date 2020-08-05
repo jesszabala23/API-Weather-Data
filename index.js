4f346add403bc83fd7690687a7b44940f70d1ad7
jesszabala23 <jzabala23@gmail.com>
2020-08-05 02:31:59 -0400

    implemented all three temparture options

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+
+
 // Assign Weather information variables
 var input = document.querySelector('.input_text');
 var main = document.querySelector('#name');
@@ -8,12 +10,14 @@ var humid = document.querySelector('.humid');
 var wind = document.querySelector('.wind');
 var max = document.querySelector('.max');
 var clouds = document.querySelector('.clouds');
-var button= document.querySelector('.submit');
+var buttonCel = document.querySelector('.celSub');
+var buttonFar = document.querySelector('.farSub');
+var buttonKel = document.querySelector('.kelSub');
 
-// On click grab client input and populate weather information.
+// On "Celsius" click grab client input and populate weather information.
 // Catch error if client input is blank
-button.addEventListener('click', function(name){
-fetch('https://api.openweathermap.org/data/2.5/weather?q='+ input.value +'&units=imperial&appid=84e036245b91790ad96fdb43d4e28993')
+buttonCel.addEventListener('click', function(name){
+fetch('https://api.openweathermap.org/data/2.5/weather?q='+ input.value +'&units=metric&appid=84e036245b91790ad96fdb43d4e28993')
 .then(response => response.json())
 .then(data => {
   var nameValue = data['name'];
@@ -39,8 +43,63 @@ fetch('https://api.openweathermap.org/data/2.5/weather?q='+ input.value +'&units
 .catch(err => alert("Oops! You forgot to type in a city."));
 })
 
+// On "Fahrenheit" click grab client input and populate weather information.
+// Catch error if client input is blank
+buttonFar.addEventListener('click', function(name){
+  fetch('https://api.openweathermap.org/data/2.5/weather?q='+ input.value +'&units=imperial&appid=84e036245b91790ad96fdb43d4e28993')
+  .then(response => response.json())
+  .then(data => {
+    var nameValue = data['name'];
+    var tempValue = data['main']['temp'];
+    var descValue = data['weather'][0]['main'];
+    var tempValue = data['main']['feels_like'];
+    var humidValue = data['main']['humidity'];
+    var windValue = data['wind']['speed'];
+    var maxValue = data['main']['temp_max'];
+  
+    main.innerHTML = nameValue;
+  
+    temp.innerHTML = tempValue;
+    desc.innerHTML = descValue;
+    feels.innerHTML = "Feels Like: "+ tempValue;
+    humid.innerHTML = "Humidity: "+ humidValue;
+    wind.innerHTML = "Wind Speed: "+ windValue;
+    max.innerHTML = "Max Temprature: "+ maxValue;
+    input.value ="";
+  
+  })
+  
+  .catch(err => alert("Oops! You forgot to type in a city."));
+  })
 
-
+// On "Kelvin" click grab client input and populate weather information.
+// Catch error if client input is blank
+buttonKel.addEventListener('click', function(name){
+  fetch('https://api.openweathermap.org/data/2.5/weather?q='+ input.value +'&appid=84e036245b91790ad96fdb43d4e28993')
+  .then(response => response.json())
+  .then(data => {
+    var nameValue = data['name'];
+    var tempValue = data['main']['temp'];
+    var descValue = data['weather'][0]['main'];
+    var tempValue = data['main']['feels_like'];
+    var humidValue = data['main']['humidity'];
+    var windValue = data['wind']['speed'];
+    var maxValue = data['main']['temp_max'];
+  
+    main.innerHTML = nameValue;
+  
+    temp.innerHTML = tempValue;
+    desc.innerHTML = descValue;
+    feels.innerHTML = "Feels Like: "+ tempValue;
+    humid.innerHTML = "Humidity: "+ humidValue;
+    wind.innerHTML = "Wind Speed: "+ windValue;
+    max.innerHTML = "Max Temprature: "+ maxValue;
+    input.value ="";
+  
+  })
+  
+  .catch(err => alert("Oops! You forgot to type in a city."));
+  })
 
 
 
